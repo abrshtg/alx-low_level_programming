ebaba53551ffec495286e1d210dba8286a95abf6
abrshtg <abrshtgam@gmail.com>
2021-06-17 01:52:00 -0700

    fix betty documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int main(void){
+/*Description: prints "\"Programming is like building a multilingual puzzle"
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
