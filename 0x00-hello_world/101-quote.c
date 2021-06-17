7b973fd0181d339d23e6ccb029e3985184123d7d
abrshtg <abrshtgam@gmail.com>
2021-06-17 05:16:42 -0700

    fix minor error in the code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,6 +12,6 @@ int main(void)
 	long syscall = 1;
 	long ret = 0;
 
-	__asm__("syscall" : "=a" (syscall), "D" (fd), "S" (s), "d" (l));
+	__asm__("syscall" : "=a" (ret) : "a" (syscall), "D" (fd), "S" (s), "d" (l));
 	return (1);
 }
